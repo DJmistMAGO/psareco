d3b0d6abb4f85bc2ebab100278b3722745ead442
MISTMAGO <gollenamikestephen@gmail.com>
2026-08-10 16:57:53 +0800

    Build: added SpatieRoles

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,19 +9,15 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    use HasRoles;
+    use HasFactory;
+    use Notifiable;
     protected function casts(): array
     {
         return [
